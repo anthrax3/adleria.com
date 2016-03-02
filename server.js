093133ae766fdfcfaa5baef84c9447a32336d890
Manuel Astudillo <manuel@optimalbits.com>
2016-03-03 00:02:45 +0100

    added better server path to static files

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var staticServer = require('node-static');
+var path = require('path');
 
 //
 // Create a node-static server instance to serve the './public' folder
 //
-var file = new staticServer.Server('./build/home');
+var file = new staticServer.Server(path.join(__dirname, 'build', 'home'));
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
@@ -14,3 +15,4 @@ require('http').createServer(function (request, response) {
     }).resume();
 }).listen(8080);
 
+console.log("listening port 8080");
